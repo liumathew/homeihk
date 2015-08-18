c377885bf1fa764f8a0991c4a867ba8fba559485
mathew <xiaohu1986@gmail.com>
2015-08-18 00:52:24 -0400

    add fields

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@ $app = new Silex\Application();
 date_default_timezone_set('America/Montreal');
 $app['debug'] = true;
 
-$app->register(new Binfo\Silex\MobileDetectServiceProvider());
+//$app->register(new Binfo\Silex\MobileDetectServiceProvider());
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
 	'twig.path' => __DIR__ . '/views',
@@ -32,14 +32,18 @@ $app->register(new Silex\Provider\TranslationServiceProvider(), array(
 
 $app->match('/', function (Symfony\Component\HttpFoundation\Request $request) use ($app) {
 
-	$data = array();
+	$data = array('pick_time'=>new \Datetime());
 	$data = array_merge($data, $request->query->all());
-
+$now=new \Datetime();
 	/** @var Symfony\Component\Form\Form $form */
 	$form = $app['form.factory']->createNamedBuilder(null, 'form', $data, array('csrf_protection' => false))
 		->setAction('/')
 		->add('name', 'text', array(
-			'label' => '您',
+			'label' => '您(微信名)',
+			'required' => true
+		))
+		->add('tel', 'text', array(
+			'label' => '电话',
 			'required' => true
 		))
 	//todo:range
@@ -48,7 +52,17 @@ $app->match('/', function (Symfony\Component\HttpFoundation\Request $request) us
 			'required' => true
 		))
 
+		->add('pick_time', 'datetime', array(
+			'data' => $now->modify('next Sunday 10:00:00'),
+			'hours'=> array(9,10,11,12,1,2,3,4,5),
+			'minutes'=> array(00,15,30,45),
+			'format' => 'yyyy-MM-dd',
+			'label' => '取煎饼时间',
+			'required' => true
+		))
+
 		->add('remark', 'textarea', array(
+			'label' => '备注',
 			'required' => false
 		))
 		->add('submit', 'submit', array('label' => '订上'))
